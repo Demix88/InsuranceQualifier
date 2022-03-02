49ef7f195f312cc506b09672352b22a1a56aa7a1
Demix88 <skrasnauskis@gmail.com>
2022-03-02 09:54:46 +0000

    update1

---
+++ InsuranceQualifier/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceQualifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int age = Convert.ToInt32(yourAge);
+            Console.WriteLine("Have you ever had a DUI Violation? (i.e.,driving under alchohol influence).Please write true or false ");
+            string haveDui = Console.ReadLine();
+            bool dui = Convert.ToBoolean(haveDui);
+            Console.WriteLine("How many speeding tickets do yo have?");
+            string speedTickets = Console.ReadLine();
+            int tickets = Convert.ToInt32(speedTickets);
+            Console.WriteLine(age > 15 && tickets < 3 && dui == false);
+            Console.ReadLine();
+
+        }
+    }
+}
